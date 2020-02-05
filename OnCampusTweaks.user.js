9bcbeb0b7324418c868da7dd89fef2d0c73611e9
Zack Sargent <46602241+zsarge@users.noreply.github.com>
2020-02-04 21:29:09 -0500

    add temporary fix for half cleared assignments

--- OnCampusTweaks.user.js
+++ OnCampusTweaks.user.js
@@ -1,21 +1,25 @@
 // ==UserScript==
 // @name         OnCampusTweaks
 // @namespace    https://elderhs.myschoolapp.com
-// @version      0.8
+// @version      0.9
 // @description  Change the way Elder High School's student website behaves.
 // @author       Zack Sargent
 // @match        https://elderhs.myschoolapp.com/app/student
 // @grant        none
 // ==/UserScript==
+// THIS CODE IS RELEASED UNDER THE MIT LICENSE
 
 // Runs when page is fully loaded
 // The website is set up in a way that window.onload triggers before the page is fully loaded.
 // Thus, we must check independently to see if the full page has loaded.
 var checkExist = setInterval(function() {
-    if (document.readyState === 'ready' || document.readyState === 'complete') {
-        changeToWeekView();
-        hideCompletedTasks();
-        clearInterval(checkExist);
+    // if the string "assignment-center" is in the url
+    if (window.location.href.indexOf("assignment-center") > -1) { // TODO: find better way of doing this
+        if (document.readyState === 'ready' || document.readyState === 'complete') {
+            changeToWeekView();
+            hideCompletedTasks();
+            clearInterval(checkExist);
+        }
     }
 }, 100); // check every 100ms
 
@@ -44,6 +48,14 @@ function hideCompletedTasks() {
 
         document.getElementById("btn-filter-apply").click();
 
+        submitToVerify();
+
         console.log("OnCampusTweaks: Hid completed tasks");
     }
+}
+
+// Sometimes the built in function does not work very well, so we have to try again.
+function submitToVerify() {
+    document.getElementById("filter-status").click();
+    document.getElementById("btn-filter-apply").click();
 }
