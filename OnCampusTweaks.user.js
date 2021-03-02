7fa57a85a61950a7bad849be6a13a5cc78fa63ca
N3UTR1N0 <n3utr1n000@gmail.com>
2021-03-02 16:58:58 -0600

    Update to merge GPA and a few extra things
    Merges GPA, adds auto show, actually correctly clicks the buttons for auto complete hide

--- OnCampusTweaks.user.js
+++ OnCampusTweaks.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         OnCampusTweaks
-// @namespace    https://elderhs.myschoolapp.com
-// @version      0.9
-// @description  Change the way Elder High School's student website behaves.
-// @author       Zack Sargent
-// @match        https://elderhs.myschoolapp.com/app/student
+// @namespace    https://*.myschoolapp.com
+// @version      0.9.1
+// @description  Change the way any myschoolappp based student website behaves.
+// @author       Zack Sargent & N3
+// @match        https://*.myschoolapp.com/app/*
 // @grant        none
 // ==/UserScript==
 // THIS CODE IS RELEASED UNDER THE MIT LICENSE
@@ -14,37 +14,35 @@
 // Thus, we must check independently to see if the full page has loaded.
 var checkExist = setInterval(function() {
     // if the string "assignment-center" is in the url
-    if (window.location.href.indexOf("assignment-center") > -1) { // TODO: find better way of doing this
+    if (window.location.href.indexOf("assignment-center") > -1) {
         if (document.readyState === 'ready' || document.readyState === 'complete') {
             changeToWeekView();
             hideCompletedTasks();
-            clearInterval(checkExist);
+            clearInterval(checkExist); //Run only one time. not needed to run again as the website saves your preferences for that session
         }
     }
-}, 100); // check every 100ms
-
-// Changes to week view
+},100)//100 ms
 function changeToWeekView() {
     // if the string "assignment-center" is in the url
     if (window.location.href.indexOf("assignment-center") > -1) {
         document.getElementById("week-view").click();
         console.log("OnCampusTweaks: Changed to week view");
     }
-
 };
 
 // hides completed tasks automatically in the assignment center
+// Unfortunately upon marking a assignment as completed, it will not dissapear however I am not going to mess with this as its most likely by design of the website and some people could prefer to still see it.
 function hideCompletedTasks() {
     if (window.location.href.indexOf("assignment-center") > -1) {
         document.getElementById("filter-status").click();
 
         // Create a list of all of the button elements that appear when you filter by status
         var buttonElements = document.getElementsByClassName("pull-left btn btn-xs btn-approve status-button active");
-        buttonElements[3].click(); // Hides Completed tasks
+        buttonElements[4].click(); // Hides Completed tasks
 
         // We have to get the buttons again because clicking on them changes the class structure
         buttonElements = document.getElementsByClassName("pull-left btn btn-xs btn-approve status-button active");
-        buttonElements[3].click(); // Hides Graded tasks
+        buttonElements[4].click(); // Hides Graded tasks
 
         document.getElementById("btn-filter-apply").click();
 
@@ -53,9 +51,75 @@ function hideCompletedTasks() {
         console.log("OnCampusTweaks: Hid completed tasks");
     }
 }
-
+function AlwaysShowGrades() {
+    if (window.location.href.indexOf("progress") > -1) {
+        document.getElementById("showGrade").click();
+        console.log("OnCampusTweaks: Grades Shown");
+    }
+}
 // Sometimes the built in function does not work very well, so we have to try again.
 function submitToVerify() {
     document.getElementById("filter-status").click();
     document.getElementById("btn-filter-apply").click();
+}
+// -------------------
+// GPA Calculator
+// -------------------
+
+var checkGrades = setInterval(function() {
+    // if the string "assignment-center" is in the url
+    if (window.location.href.indexOf("progress") > -1) { // TODO: find better way of doing this
+        if (document.readyState === 'ready' || document.readyState === 'complete') {
+            let courseDiv = document.getElementById("courses")
+            let gradesArray = courseDiv.innerText.match(/([0-9][0-9][0-9]|[0-9][0-9])\%/gi);
+            showGradeAverage(gradesArray);
+            AlwaysShowGrades();
+        }
+    }
+}, 500); // check every 500ms
+
+function showGradeAverage(gradesArray) {
+    if (gradesArray) {
+        runOnce(() => {
+            let gpa = getGPA(gradesArray)
+            getGPADiv().textContent += `Grade Average: ${ gpa }%\n | `
+            for(var i = 0; i < gradesArray.length; i++) {
+                    gradesArray[i] = parseInt(gradesArray[i]);
+                }
+                let min = Math.min(...gradesArray)
+                getGPADiv().textContent += `Lowest grade: ${ min }%\n | `
+                let max = Math.max(...gradesArray)
+                getGPADiv().textContent += `Highest grade: ${ max }%\n |`
+        }
+               )}
+}
+var hasRun = false;
+function runOnce(func) {
+    if (!hasRun) {
+        func();
+        hasRun = true;
+    }
+}
+
+function getGPA(gradesArray) {
+    const average = (array) => array.reduce((a, b) => a + b) / array.length;
+
+    // turn array of strings into array of ints
+    for(var i = 0; i < gradesArray.length; i++) {
+        gradesArray[i] = gradesArray[i].replace('\%', '');
+        gradesArray[i] = parseInt(gradesArray[i]);
+    }
+
+    let gpa = average(gradesArray)
+    return Math.round(gpa * 100) / 100
+    // round to two decimal places
+}
+
+function getGPADiv() {
+    // Re-purposes unused Performance Div
+    let div = document.createElement("div")
+    div.id = "custom-grades"
+    let performance = document.getElementById("performanceCollapse")
+    performance.appendChild(div)
+    return document.getElementById("custom-grades")
 }
